6f6ab668b9839db7422e1816bd3cde35ee838f26
Canaan Epperson <cepperson16@georgefox.edu>
2020-03-31 15:12:24 -0700

    compiled

---
+++ pipe.js
@@ -0,0 +1,44 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var Pipe = /** @class */ (function (_super) {
+    __extends(Pipe, _super);
+    function Pipe() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    Pipe.prototype.Init = function (world) {
+        this.element.css("background-color", "green");
+        this.position.x = MAX_WIDTH;
+        this.collidable = true;
+        this.size.x = 150;
+    };
+    Pipe.prototype.Pre = function (world) {
+        this.velocity.x = Pipe.speed;
+    };
+    Pipe.prototype.Post = function (world) {
+        // If pipe has left frame, delete it from the world and remove it from the DOM.
+        if (this.position.x + this.size.x < 0) {
+            this.toDelete = true;
+            this.collidable = false;
+        }
+    };
+    Pipe.prototype.Collision = function (world) {
+        this.collisions.forEach(function (collision) {
+            if (collision.transform instanceof Bird) {
+                Game.state = State.gameOver;
+            }
+        });
+    };
+    Pipe.speed = -200;
+    return Pipe;
+}(Transform));
